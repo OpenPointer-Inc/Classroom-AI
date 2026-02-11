42030c20b80b30b35f4be4ccc52a8a3ae6f5c284
Faseeh Ahmad <faseehkamboh9933@gmail.com>
2026-02-11 16:16:23 +0500

    Get Started button FIXED

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -91,9 +91,9 @@ export default function Navigation() {
         <div className="flex items-center gap-4">
           <div className="mt-4 hidden lg:flex w-[105.9px] xl:w-[123.2px]">
             <Link
-              aria-label="Go to register page"
+              aria-label="Go to app"
               className="group relative inline-flex items-center justify-start overflow-hidden rounded-full px-3 py-2 font-bold md:px-4 lg:px-5 lg:py-3"
-              href="/register"
+              href="https://app.classroomai.site"
             >
               <span className="absolute left-0 top-0 h-32 w-32 -translate-y-2 translate-x-12 rotate-45 bg-[#1CD98E] opacity-[3%]"></span>
               <span className="absolute left-0 top-0 -mt-1 h-48 w-48 -translate-x-56 -translate-y-24 rotate-45 bg-[#1CD98E] opacity-100 transition-all duration-500 ease-in-out group-hover:-translate-x-8"></span>
