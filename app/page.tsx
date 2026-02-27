b8217f83b07b641cc8c2d0775546208a598fb2a5
Faseeh Ahmad <faseehkamboh9933@gmail.com>
2026-02-27 07:29:27 +0500

    Optimization  Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import dynamic from 'next/dynamic';
 import Footer from '@/components/Footer';
 import CalltoAction from '@/components/CalltoAction';
-import Navigation from '@/components/Navigation';
 import FeatureSection from '@/components/FeatureSection';
 import Top from '@/components/Top';
 import Faqs from '@/components/Faqs';
-import AnimatedDiv from '@/components/AnimatedDiv';
-import Howitworks from '@/components/Howitworks';
+const AnimatedDiv = dynamic(() => import('@/components/AnimatedDiv'));
+const Howitworks = dynamic(() => import('@/components/Howitworks'));
 export default function Home() {
   return (
     <main className="min-h-screen">
